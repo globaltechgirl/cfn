d9d5fcb492113b60497739943689da61ec1ad53f
onyinyeofili <onyinyeofili209@gmail.com>
2025-10-21 04:29:01 +0100

    Updated

--- src/component/about/presidentsData.ts
+++ src/component/about/presidentsData.ts
@@ -2,8 +2,8 @@ import President1 from "@/assets/president1.webp";
 import President2 from "@/assets/president2.webp";
 import President3 from "@/assets/president3.webp";
 import President4 from "@/assets/president4.jpg";
-import President7 from "@/assets/president7.webp";
-import President8 from "@/assets/president8.webp";
+import President7 from "@/assets/president7.jpg";
+import President8 from "@/assets/president8.jpg";
 import President9 from "@/assets/president9.webp";
 
 export interface President {
